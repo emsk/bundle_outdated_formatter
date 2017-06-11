71c8a0949d59e50a48fdb0e6b6a25e45014ec3d9
emsk <emsk1987@gmail.com>
2017-06-11 17:06:16 +0900

    Add specs for `BundleOutdatedFormatter::CLI`

--- spec/bundle_outdated_formatter/cli_spec.rb
+++ spec/bundle_outdated_formatter/cli_spec.rb
@@ -1,2 +1,163 @@
 RSpec.describe BundleOutdatedFormatter::CLI do
+  let(:stdin) do
+    <<-EOS
+Fetching gem metadata from https://rubygems.org/..........
+Fetching version metadata from https://rubygems.org/...
+Fetching dependency metadata from https://rubygems.org/..
+Resolving dependencies...
+
+Outdated gems included in the bundle:
+* faker (newest 1.6.6, installed 1.6.5, requested ~> 1.4) in groups "development, test"
+* hashie (newest 3.4.6, installed 1.2.0, requested = 1.2.0) in groups "default"
+* headless (newest 2.3.1, installed 2.2.3)
+    EOS
+  end
+
+  let(:stdout_markdown) do
+    <<-EOS
+| gem | newest | installed | requested | groups |
+| --- | --- | --- | --- | --- |
+| faker | 1.6.6 | 1.6.5 | ~> 1.4 | development, test |
+| hashie | 3.4.6 | 1.2.0 | = 1.2.0 | default |
+| headless | 2.3.1 | 2.2.3 | | |
+    EOS
+  end
+
+  let(:help) do
+    <<-EOS
+Commands:
+  #{command} help [COMMAND]  # Describe available commands or one specific command
+  #{command} output          # Format output of `bundle outdated`
+
+    EOS
+  end
+
+  shared_examples_for 'markdown format' do
+    before do
+      stub_const('STDIN', StringIO.new(stdin))
+    end
+
+    it { is_expected.to output(stdout_markdown).to_stdout }
+  end
+
+  shared_examples_for 'unknown format' do
+    before do
+      stub_const('STDIN', StringIO.new(stdin))
+    end
+
+    it { is_expected.not_to output.to_stdout }
+  end
+
+  shared_examples_for 'a `help` command' do
+    before do
+      expect(File).to receive(:basename).with($PROGRAM_NAME).and_return(command).at_least(:once)
+    end
+
+    it { is_expected.to output(help).to_stdout }
+  end
+
+  describe '.start' do
+    let(:command) { 'bundle_outdated_formatter' }
+
+    subject { -> { described_class.start(thor_args) } }
+
+    context 'given `output --format markdown`' do
+      let(:thor_args) { %w[output --format markdown] }
+      it_behaves_like 'markdown format'
+
+      context 'without STDIN' do
+        it { is_expected.not_to output.to_stdout }
+      end
+    end
+
+    context 'given `output -f markdown`' do
+      let(:thor_args) { %w[output -f markdown] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given `output`' do
+      let(:thor_args) { %w[output] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given ``' do
+      let(:thor_args) { %w[] }
+      it_behaves_like 'markdown format'
+    end
+
+    context 'given `output` --format aaa' do
+      let(:thor_args) { %w[output --format aaa] }
+      it_behaves_like 'unknown format'
+    end
+
+    context 'given `help`' do
+      let(:thor_args) { %w[help] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `--help`' do
+      let(:thor_args) { %w[--help] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `-h`' do
+      let(:thor_args) { %w[-h] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `h`' do
+      let(:thor_args) { %w[h] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `he`' do
+      let(:thor_args) { %w[he] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `hel`' do
+      let(:thor_args) { %w[hel] }
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `help output`' do
+      let(:thor_args) { %w[help output] }
+      let(:help) do
+        <<-EOS
+Usage:
+  #{command} output
+
+Options:
+  -f, [--format=FORMAT]  
+                         # Default: markdown
+
+Format output of `bundle outdated`
+        EOS
+      end
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `help help`' do
+      let(:thor_args) { %w[help help] }
+      let(:help) do
+        <<-EOS
+Usage:
+  #{command} help [COMMAND]
+
+Describe available commands or one specific command
+        EOS
+      end
+      it_behaves_like 'a `help` command'
+    end
+
+    context 'given `abc`' do
+      let(:thor_args) { %w[abc] }
+      it { is_expected.to output(%(Could not find command "abc".\n)).to_stderr }
+    end
+
+    context 'given `helpp`' do
+      let(:thor_args) { %w[helpp] }
+      it { is_expected.to output(%(Could not find command "helpp".\n)).to_stderr }
+    end
+  end
 end
