6337ccf6c08b78a30c5cbe6908383885b45daa32
emsk <emsk1987@gmail.com>
2019-10-24 22:13:36 +0900

    Try to fix failing tests on AppVeyor
    * https://ci.appveyor.com/project/emsk/bundle-outdated-formatter/builds/28350626
    * Suppress output in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,4 +23,15 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  original_stdout = $stdout
+  original_stderr = $stderr
+  config.before(:suite) do
+    $stdout = File.open(File::NULL, 'w')
+    $stderr = File.open(File::NULL, 'w')
+  end
+  config.after(:suite) do
+    $stdout = original_stdout
+    $stderr = original_stderr
+  end
 end
